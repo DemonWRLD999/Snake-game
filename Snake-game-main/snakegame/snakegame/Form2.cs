9abdaa8cc116d44093c6a815eaabc4ab9a563122
Kuba2037 <kuba123bor@gmail.com>
2026-05-14 12:28:08 +0200

    interfejs dziala

---
+++ Snake-game-main/snakegame/snakegame/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace snakegame
+{
+    public partial class Form2 : Form
+    {
+        bool fs = true;
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            Load += Form2_Load;
+            KeyPreview = true;
+            KeyDown += Form2_KeyDown;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            SetFullscreen(true);
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F11)
+                SetFullscreen(!fs);
+        }
+
+        void SetFullscreen(bool on)
+        {
+            fs = on;
+
+            FormBorderStyle = on ? FormBorderStyle.None : FormBorderStyle.Sizable;
+            WindowState = on ? FormWindowState.Maximized : FormWindowState.Normal;
+            TopMost = on;
+        }
+    }
+}
