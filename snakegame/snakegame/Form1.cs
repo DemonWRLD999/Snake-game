c25b8569e8f348bddcee0e557cf9acfe22e39cd4
DemonWRLD999 <mikolaj.baj@outlook.com>
2026-05-14 09:54:26 +0200

    oprawa wizualna projektu
    ...

---
+++ snakegame/snakegame/Form1.cs
@@ -0,0 +1,16 @@
+namespace snakegame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
